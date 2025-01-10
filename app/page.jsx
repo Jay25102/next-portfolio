3ded36020f7166dd741a35ee48dc38c9c7392fa5
jay25102 <jasonhuang25102@gmail.com>
2025-01-10 17:18:59 -0500

    text centered on home page

--- app/page.jsx
+++ app/page.jsx
@@ -5,16 +5,18 @@ const Home = () => {
   return (
     <div>
         {/* left and right bars */}
-        <div className="bar bar-left bg-blue-cust"></div>
-        <div className="bar bar-right bg-blue-cust"></div>
+        <div className="bar bar-left bg-white-cust"></div>
+        <div className="bar bar-right bg-white-cust"></div>
         {/* header component */}
         <Header/>
         {/* body */}
         <div className="box-border block bg-light-blue-cust">
-          <div className="box-border block body-block px-8 relative">
+          <div className="box-border block body-block px-8 relative flex justify-center items-center">
               {/* make explore/share links */}
               {/* center */}
-              <h1 className="text-7xl font-bold text-white-cust w-auto block w-1/2">Explore and Share Desktop Wallpapers</h1>
+              <h1 className="text-7xl font-bold text-white-cust text-center align-middle w-4/5 h-auto">
+                <span className="text-orange-cust">Explore</span> and <span className="text-blue-cust">Share</span> Desktop Wallpapers
+              </h1>
           </div>
         </div>
         {/* footer */}
